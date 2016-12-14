fa37e27bce36d8338d6a4b079e95f93986d3b221
Moises Silva <mogosilva@gmail.com>
2016-12-14 17:30:10 -0500

    First Commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function checkWidth(){
+  if ($(window).width() < 900){
+    $(".home header").show();
+    $(".static").hide();
+  }else {
+    $(".static").show();
+    if($(window).scrollTop() > 580){
+      $(".home header").show();
+    }else{
+      $(".home header").hide();
+    }
+  }
+}
+
+
+
+$(document).ready(function(){
+  // check screen width as soon as page loads
+  checkWidth();
+
+  //everytime you scroll, check screen width
+  $(window).on("scroll",function(){
+    checkWidth();
+  });
+
+
+
+
+
+});
